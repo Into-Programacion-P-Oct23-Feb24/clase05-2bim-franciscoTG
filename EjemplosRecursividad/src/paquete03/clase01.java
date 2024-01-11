cc801246cc29ee9fbfdb93c12f4b7956db958df1
Francisco Torres <ft110565752@gmail.com>
2024-01-11 15:52:09 -0500

    ejercicio

---
+++ EjemplosRecursividad/src/paquete03/clase01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author ft110
+ */
+public class clase01 {
+
+    public static void main(String[] arg) {
+    }
+
+    public static String[][] obtenerCadena() {
+     String [][] cadena={{"LOJA","CUENCA","QUITO"}
+             
+     ,{"GUAYAQUIL","ZAMORA","MACHALA"}};
+     return cadena;
+    }
+}
+
